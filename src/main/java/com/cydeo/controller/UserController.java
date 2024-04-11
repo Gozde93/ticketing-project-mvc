33b6af0248c3de18c94e8da3aaa391cb204f9209
gozdediri <dirigozde@gmail.com>
2024-04-11 01:58:15 -0500

    Ticketing part 2

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,6 +6,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,4 +34,18 @@ public class UserController {
 
         return "/user/create";
     }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user")  UserDTO user,Model model){
+        model.addAttribute("user", new UserDTO());
+
+        model.addAttribute("roles",roleService.findAll() );
+
+        userService.save(user);
+
+        model.addAttribute("users", userService.findAll() );
+
+        return "/user/create";
+    }
+
 }
---
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -0,0 +1,13 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.RoleDTO;
+import org.springframework.core.convert.converter.Converter;
+
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
+
+
+    @Override
+    public RoleDTO convert(String source) {
+        return null;
+    }
+}
