1904473c3984eb348cc20b05f0e6768d9fd796ee
gozdediri <dirigozde@gmail.com>
2024-04-22 20:01:47 -0500

    Ticketing part 4

--- src/main/java/com/cydeo/converter/UserDtoConverter.java
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -2,10 +2,12 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+//import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+//@ConfigurationPropertiesBinding
 public class UserDtoConverter implements Converter<String, UserDTO> {
 
     UserService userService;
@@ -16,7 +18,13 @@ public class UserDtoConverter implements Converter<String, UserDTO> {
 
     @Override
     public UserDTO convert(String source) {
+
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
         return userService.findById(source);
+
     }
 
 }
--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -5,37 +5,42 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-
+import javax.validation.constraints.*;
 
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
 
-
+    @NotBlank
+    @Size(max = 15, min = 2)
     private String firstName;
 
-
+    @NotBlank
+    @Size(max = 15, min = 2)
     private String lastName;
 
-
+    @NotBlank
+    @Email
     private String userName;
 
-
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String passWord;
 
-
+    @NotNull
     private String confirmPassWord;
 
     private boolean enabled;
 
-
+    @NotBlank
+    @Pattern(regexp = "^\\d{10}$")
     private String phone;
 
-
+    @NotNull
     private RoleDTO role;
 
-
+    @NotNull
     private Gender gender;
 
     public String getPassWord() {
